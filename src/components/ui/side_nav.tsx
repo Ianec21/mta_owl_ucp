2e03c244abc1876f5ef47fd1a9afad607f8bb64f
Ianec21 <jizdan.ianec17@gmail.com>
2024-04-04 02:08:03 +0300

    finished homepage for now

--- src/components/ui/side_nav.tsx
+++ src/components/ui/side_nav.tsx
@@ -14,15 +14,20 @@ const SideNavigation = () => {
 
   if(pathname !== "/sign-in" && pathname !== "/sign-up"){
     return (
-      <nav className="bg-secondary rounded-md flex flex-col w-[350px] h-screen">
+      <nav className="bg-secondary rounded-md flex flex-col w-[350px]">
         <div className="p-5 bg-secondary" style={{
           borderTopLeftRadius: 5
         }}>
           <p className="text-primary font-bold text-xl">MTA UCP OWL</p>
         </div>
   
-        <div className="flex flex-col p-5 gap-3">
-          {navButtons.map(button => <SideNavigationButton key={button.id} title={button.title} href={button.href} current={pathname === button.href}/>)}
+        <div className="flex flex-col p-2 gap-2">
+          {navButtons.map(button => 
+            <div key={button.id} className="relative">
+              { pathname === button.href && <div className="absolute rounded-md w-full h-full bg-primary opacity-10"/>}
+              <SideNavigationButton title={button.title} href={button.href} current={pathname === button.href}/>
+            </div>
+            )}
         </div>
       </nav>
     )
@@ -35,8 +40,9 @@ const SideNavigationButton = ({
   current
 } : { title: string, href: string, current: boolean } ) => {
   return (
-    <div className={`hover:cursor-pointer ${current && "border-s-2 pl-2"} border-orange-400 flex items-center`}>
-      <Link href={href}>{ title }</Link>
+    <div className={`hover:cursor-pointer flex items-center relative p-2`}>
+      { current && <div className="bg-primary w-[5px] h-[5px] rounded-lg mr-2"/>}
+      <Link className="text-2sm" href={href}>{ title }</Link>
     </div>
   )
 }
